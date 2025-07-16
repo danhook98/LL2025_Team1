4e4a18a33f3cbf78dd2cbe947575e81aa59309b5
Dan Hook <s188873@uos.ac.uk>
2025-07-16 14:54:30 +0100

    Validate audio sources on awake

--- Assets/_Scripts/Audio/AudioManager.cs
+++ Assets/_Scripts/Audio/AudioManager.cs
@@ -4,6 +4,27 @@ namespace CannonGame.Audio
 {
     public class AudioManager : MonoBehaviour
     {
-        
+        [SerializeField] private AudioSource audioSourceSFX;
+        [SerializeField] private AudioSource audioSourceMusic;
+
+        private void Awake()
+        {
+            // Tried creating a method to verify the audio sources but Unity threw a hissy fit, so manual it is!
+            if (!audioSourceSFX)
+            {
+                Debug.LogWarning("<color=red>AudioManager</color>: SFX audio source not assigned or is invalid, " +
+                    "creating a temporary one. Expect weird behaviour.");
+
+                audioSourceSFX = gameObject.AddComponent<AudioSource>();
+            }
+
+            if (!audioSourceMusic)
+            {
+                Debug.LogWarning("<color=red>AudioManager</color>: music audio source not assigned or is invalid, " +
+                    "creating a temporary one. Expect weird behaviour.");
+
+                audioSourceMusic = gameObject.AddComponent<AudioSource>();
+            }
+        }
     }
 }
