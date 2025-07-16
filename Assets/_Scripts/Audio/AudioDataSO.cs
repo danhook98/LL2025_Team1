bfddc0996de7dc4a5357332af813f65155342d6a
Dan Hook <s188873@uos.ac.uk>
2025-07-16 14:58:19 +0100

    Set default audio data volume

--- Assets/_Scripts/Audio/AudioDataSO.cs
+++ Assets/_Scripts/Audio/AudioDataSO.cs
@@ -8,6 +8,6 @@ namespace CannonGame.Audio
     public class AudioDataSO : ScriptableObject
     {
         public AudioClip clip;
-        [Range(0f, 1f)] public float volume;
+        [Range(0f, 1f)] public float volume = 1f;
     }
 }
