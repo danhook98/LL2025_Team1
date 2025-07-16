1c5fb3a50cadda62b8c3bf29a1a81cb672c0e4c5
Dan Hook <s188873@uos.ac.uk>
2025-07-16 12:39:48 +0100

    Create audio data scriptable object

---
+++ Assets/_Scripts/Audio/AudioDataSO.cs
@@ -0,0 +1,13 @@
+using CannonGame.EventSystem;
+using UnityEngine;
+
+namespace CannonGame.Audio
+{
+    [CreateAssetMenu(fileName = "Audio Data",
+                     menuName = "CannonGame/Audio/Audio Data")]
+    public class AudioDataSO : ScriptableObject
+    {
+        public AudioClip clip;
+        [Range(0f, 1f)] public float volume;
+    }
+}
