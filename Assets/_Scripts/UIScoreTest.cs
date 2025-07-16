4fd8273a0a3396faec1db8208384dd51875dd3ab
MrMaker875 <ashtonrobinsonclarke@gmail.com>
2025-07-16 15:05:42 +0100

    finishing up rough UI
    added extra comments to prevent confusion when polished later

--- Assets/_Scripts/UIScoreTest.cs
+++ Assets/_Scripts/UIScoreTest.cs
@@ -2,20 +2,23 @@ using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using TMPro;
 using System.Collections;
+using Unity.VisualScripting;
 
 namespace CannonGame
 {
     public class UIScoreTest : MonoBehaviour
     {
+        //needed to disable powerup text to hide it
         public GameObject Poweruptxt;
         public GameObject Getpoweruptxt;
-
+        
+        //gives access to the UI text
         public TMP_Text Score;
         public TMP_Text Highscore;
         public TMP_Text PowerupNameText;
         public TMP_Text GainedPowerupText;
 
-
+        //keeps check on the scores
         public float Totalscore = 0f;
 
         public float Totalhighscore;
@@ -35,9 +38,8 @@ namespace CannonGame
 
             Highscore.text = Totalhighscore.ToString();
 
-
-            ///sets players highscore as current score (will make it only update on a higher score then current highscore later)
-            PlayerPrefs.SetFloat("highscore", Totalscore);
+            ///sets players highscore as current score if its higher then the current highscore
+            if (Totalscore > Totalhighscore) PlayerPrefs.SetFloat("highscore", Totalscore);
         }
 
         //changes score for testing
@@ -53,17 +55,29 @@ namespace CannonGame
         {
             Totalscore = 0f;
         }
+        public void ResetHighscore()
+        {
+            Totalhighscore = 0f;
+            //once resetted won't remember its previous highscore
+            PlayerPrefs.SetFloat("highscore", 0);
+        }
 
-        //sorting out a base way to tell the player they collected a powerup if we go random powerup spawn for this
+        //sorting out a base to tell the player they collected a powerup if we go random powerup spawn for this
         public void PowerupGet()
         {
+            //tests if it can change text while disabled and will be made to say the name of the specific powerup you collect
             PowerupNameText.text = "Splitshot";
             Poweruptxt.SetActive(true);
             Getpoweruptxt.SetActive(true);
-            //use coroutines to hide text after a duration of being active
-            //StartCoroutine(HidePowerup(5f));
+            StartCoroutine(Hidepowerup());
         }
 
-
+        //hides powerup text after time
+        IEnumerator Hidepowerup()
+        {
+            yield return new WaitForSeconds(5f);
+            Poweruptxt.SetActive(false);
+            Getpoweruptxt.SetActive(false);
+        }
     }
 }
