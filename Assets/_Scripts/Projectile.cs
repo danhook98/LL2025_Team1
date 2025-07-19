03890d60111abdae03abcf8033a13ca8f3c6b51f
Dan Hook <s188873@uos.ac.uk>
2025-07-19 20:52:52 +0100

    Add required components to projectiles script

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -2,18 +2,9 @@ using UnityEngine;
 
 namespace CannonGame
 {
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
     public class Projectile : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
