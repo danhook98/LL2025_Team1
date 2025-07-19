d5feebb2419587a891363f6f7b1338adaf0d2d84
Dan Hook <s188873@uos.ac.uk>
2025-07-19 21:01:25 +0100

    Expose launch speed in the inspector

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -5,7 +5,7 @@ namespace CannonGame
     [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
     public class Projectile : MonoBehaviour
     {
-        private float _launchSpeed;
+        [SerializeField] private float launchSpeed;
 
         private Rigidbody2D _rigidbody2d;
 
